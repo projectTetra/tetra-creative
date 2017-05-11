e71b2bd27254f5357241cec323069a440d845283
Bradley Lyman <lyman.brad3211@gmail.com>
2017-05-10 21:44:45 -0700

    create basic SDL2 init in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <SDL.h>
 
 using namespace std;
 
 int main()
 {
     cout << "hello world" << endl;
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    {
+        cout << "failed to initialize sdl" << endl;
+    }
+
+    SDL_Quit();
+    cout << "quit sdl" << endl;
 
     return 0;
 }
