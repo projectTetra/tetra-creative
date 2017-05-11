0254a0d7ead13e096c37d3d86d92820d805dd318
Bradley Lyman <lyman.brad3211@gmail.com>
2017-05-10 22:13:18 -0700

    update main.cpp to use sdl class

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,13 @@
 #include <iostream>
-#include <SDL.h>
+#include <SDL.hpp>
 
 using namespace std;
 
 int main()
 {
+    tetra::SDL sdl{};
+
     cout << "hello world" << endl;
-
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
-    {
-        cout << "failed to initialize sdl" << endl;
-    }
-
-    SDL_Quit();
-    cout << "quit sdl" << endl;
 
     return 0;
 }
