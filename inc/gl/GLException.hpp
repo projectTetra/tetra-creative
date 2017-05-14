7a095398be60dcf8c43bcd9c1a0393d66921c3ed
Bradley Lyman <lyman.brad3211@gmail.com>
2017-05-14 15:10:19 -0700

    add THROW_ON_GL_ERROR and clean error reporting

--- inc/gl/GLException.hpp
+++ inc/gl/GLException.hpp
@@ -3,10 +3,18 @@
 
 #include <exception>
 #include <initializer_list>
+#include <vector>
 #include <string>
 
 namespace tetra
 {
+    /**
+     * Throw a GLException if there is an error in the GL.
+     * The result of glGetError is added to the messages like so:
+     * GLException: glGetError -- $error string$ -- $messages$
+     */
+    void onGlError(std::string file, std::string line);
+
     /**
      * This class is used to represent exceptions while working with OpenGL.
      */
@@ -16,6 +24,7 @@ namespace tetra
         /**
          * Construct an exception with each of the messages joined with spaces.
          */
+        GLException(std::vector<std::string> messages);
         GLException(std::initializer_list<std::string> messages);
 
         /**
@@ -27,5 +36,15 @@ namespace tetra
         std::string errorMsg;
     };
 }; /* namespace tetra */
+
+
+#define _EXPAND_GL_ERR_IMPL(file, line) \
+    tetra::onGlError(#file, #line);
+
+#define _EXPAND_GL_ERR(file, line) \
+    _EXPAND_GL_ERR_IMPL(file, line)
+
+#define THROW_ON_GL_ERROR() \
+    _EXPAND_GL_ERR(__FILE__, __LINE__)
 
 #endif
--- src/gl/GLException.cpp
+++ src/gl/GLException.cpp
@@ -1,11 +1,113 @@
 #include <gl/GLException.hpp>
+
+#include <GL/glew.h>
+
 #include <sstream>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 using namespace tetra;
 
-GLException::GLException(initializer_list<string> messages)
+namespace
+{
+    struct HumanReadable
+    {
+        string error;
+        string description;
+
+        HumanReadable() = default;
+    };
+
+    HumanReadable humanReadable(GLenum error)
+    {
+#define errcase(err, msg) \
+    case err:             \
+        return {          \
+            #err,         \
+            msg           \
+        };
+
+    switch (error)
+    {
+errcase(GL_NO_ERROR, R"msg(
+    No error has been recorded.
+    The value of this symbolic constant is guaranteed to be 0
+)msg"
+)
+errcase(GL_INVALID_ENUM, R"msg(
+   An unacceptable value is specified for an enumerated argument. The offending
+   command is ignored and has no other side effect than to set the error flag.
+)msg"
+)
+errcase(GL_INVALID_VALUE, R"msg(
+    A numeric argument is out of range. The offending command is ignored and has no
+    other side effect than to set the error flag.
+)msg"
+)
+errcase(GL_INVALID_OPERATION, R"msg(
+    The specified operation is not allowed in the current state. The offending
+    command is ignored and has no other side effect than to set the error flag.
+)msg"
+)
+errcase(GL_INVALID_FRAMEBUFFER_OPERATION, R"msg(
+    The framebuffer object is not complete. The offending command is ignored and has
+    no other side effect than to set the error flag.
+)msg"
+)
+errcase(GL_OUT_OF_MEMORY, R"msg(
+    There is not enough memory left to execute the command. The state of the GL is
+    undefined, except for the state of the error flags, after this error is
+    recorded.
+)msg"
+)
+errcase(GL_STACK_UNDERFLOW, R"msg(
+    An attempt has been made to perform an operation that would cause an internal
+    stack to underflow.
+)msg"
+)
+errcase(GL_STACK_OVERFLOW, R"msg(
+    An attempt has been made to perform an operation that would cause an internal
+    stack to overflow.
+)msg"
+)
+default:
+    stringstream ss;
+    ss << error;
+    return { "Unknown GL_ERROR"
+           , ss.str()
+           };
+        }
+#undef errcase
+    }
+}
+
+void
+tetra::onGlError(string file, string line)
+{
+    auto humanErrors = vector<HumanReadable>{};
+    auto rawError = glGetError();
+    while (rawError != GL_NO_ERROR)
+    {
+        humanErrors.push_back(humanReadable(rawError));
+        rawError = glGetError();
+    }
+
+    if (humanErrors.size() != 0)
+    {
+        auto messages = vector<string>{ file
+                                      , "[" + line + "]\n"
+                                      };
+        for (auto humanError : humanErrors)
+        {
+            messages.push_back(humanError.error);
+            messages.push_back(humanError.description);
+        }
+        throw GLException{messages};
+    }
+}
+
+GLException::GLException(vector<string> messages)
 {
     errorMsg = accumulate(
         begin(messages), end(messages),
@@ -15,6 +117,10 @@ GLException::GLException(initializer_list<string> messages)
             return fullMsg + " " + curMsg;
         });
 }
+
+GLException::GLException(initializer_list<string> messages)
+    : GLException(vector<string>{messages})
+{ }
 
 const char*
 GLException::what() const noexcept
